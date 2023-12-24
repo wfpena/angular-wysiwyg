3006732a4a9e267e4a3cb607c1c2cd2bc7dbbe8f
Wilson Pena <wilsonf.pena@gmail.com>
2023-12-24 15:14:58 -0300

    feat: build demo on docs folder
    feat: demo distribution build v1.0.0
    
    feat: post build demo script to fetch assets
    
    feat: style and documentation improvements
    
    feat: change width
    
    feat: new docs version with max width
    
    feat: config playground
    
    fix: dist to gitignore
    
    feat: demo build v1.0.7
    
    feat: add demo build to github actions
    
    fix

---
+++ postDemoBuild.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+const package = require('./package.json');
+
+fs.readFile('./docs/main.js', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replaceAll('/assets/fonts', './assets/fonts');
+
+  fs.writeFile('./docs/main.js', result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+
+  const versionJson = {
+    version: package.version,
+    name: package.name,
+    timestamp: new Date().toString(),
+  };
+  fs.writeFileSync('./docs/version.json', JSON.stringify(versionJson));
+});
