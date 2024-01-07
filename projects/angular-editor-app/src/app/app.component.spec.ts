efb78e87d4aa7af402ad05af6006f5e29cd9fc7d
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2024-01-07 10:33:36 -0300

    feat: adds unit tests run to github actions workflow

--- projects/angular-editor-app/src/app/app.component.spec.ts
+++ projects/angular-editor-app/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [HttpClientModule]
     }).compileComponents();
   }));
 
@@ -16,16 +18,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-editor-app'`, () => {
+  it(`should have as title 'angular-wysiwyg-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-editor-app');
+    console.log('app her:', app);
+    expect(app.title).toEqual('angular-wysiwyg-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-editor-app!');
+    expect(compiled.querySelector('h2').textContent).toContain('Basic Example with HTML Output');
   });
 });
